23174e1340b379eb7b2f3cb1a81d12493ac05eee
Luca Micieli <rams23@live.it>
2020-05-15 00:53:03 +0200

    add navigate with params button

--- src/settings/SettingsPage.tsx
+++ src/settings/SettingsPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
-import {useRoute, useNavigation} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {HOME_TAB, PROFILE_SCREEN} from '../constants';
 
@@ -12,13 +12,30 @@ const SettingsPage: React.FC<Props> = ({}) => {
   const navigation = useNavigation<StackNavigationProp<any>>();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>SettingsPage</Text>
       <Text>params: {JSON.stringify(params, undefined, 4)}</Text>
 
-      <Button title="Navigate to Profile" onPress={()=>navigation.navigate(HOME_TAB, {
-        screen: PROFILE_SCREEN
-      })} />
+      <View style={{marginTop:20}}>
+        <Button
+          title="Navigate to Profile"
+          onPress={() => navigation.navigate(HOME_TAB, {
+            screen: PROFILE_SCREEN
+          })}
+        />
+      </View>
+
+      <View style={{marginTop:20}}>
+        <Button
+          title="Navigate to Profile with params"
+          onPress={() => navigation.navigate(HOME_TAB, {
+            screen: PROFILE_SCREEN,
+            params: {
+              foo: 'bar'
+            }
+          })}
+        />
+      </View>
     </View>
   );
 };
